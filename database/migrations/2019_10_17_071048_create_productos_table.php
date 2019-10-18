dec0ef1633b5374149c2b7ea750acde140f045ee
aperuchena <aimarperuchena@gmail.com>
2019-10-18 09:34:42 +0200

    Subir arhvivos laravel

---
+++ database/migrations/2019_10_17_071048_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('id_tienda');
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->double('precio',8,2);
+            $table->bigInteger('stock');
+            $table->string('link_imagen')->nullable();
+            $table->string('link')->nullable();
+
+            $table->foreign('id_tienda')->references('id')->on('tiendas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
