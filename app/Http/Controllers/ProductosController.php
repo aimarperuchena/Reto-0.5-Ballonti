9951d75eca0cc374f5dbb733104b520b7f3d1ca7
aperuchena <aimarperuchena@gmail.com>
2019-10-22 09:49:57 +0200

    aitor prueba

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ProductosModel;
+use Validator;
 class ProductosController extends Controller
 {
     public function listarProductos($id){
         $productos = ProductosController::table('productos')->where('id_tienda', $id);
         return view('productos',compact('productos'));
+    }
+
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+			
+            'nombre' => 'required|unique:posts|max:255|min:1',
+            'descripcion' => 'required|max:255|min:1',
+            'precio' => 'required|numeric',
+            'imagen' => 'file',
+            'link' => 'max:255'
+        ]);
+
+        if ($validator->fails()) {
+			return redirect('ProductosModel/create')
+				->withErrors($validator)
+				->withInput();
+		}
+		// Store post
+		$post = Post::create($request->except('csrf'));
+		return redirect(url('/'));
     }
 }
